b385866da1e83d74986c4d0eceb6ca61ff741ec5
wiexed <kasperden67@gmail.com>
2026-01-22 08:51:12 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "waiter-pwa-v5";
+const CACHE_NAME = "waiter-pwa-v2";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./sw.js"];
 
 self.addEventListener("install", (event) => {
@@ -22,5 +22,6 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
+
 
 
